67f74d507ff8d0a823c68a647b8dbea0781af331
Nilla Putri Rosidania <nillaputrirosidania@gmail.com>
2024-06-02 19:44:53 +0700

    first commit: UI akun, ubah nama, ubah email, ubah foto, ubah password, about us

---
+++ app/src/main/java/com/nilla/batikpedia/UbahFoto.kt
@@ -0,0 +1,11 @@
+package com.nilla.batikpedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UbahFoto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ubah_foto)
+    }
+}
---
+++ app/src/main/java/com/nilla/batikpedia/UbahPassword.kt
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+package com.nilla.batikpedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UbahPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ubah_password)
+    }
+}
